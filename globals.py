0e7062dfabd350448968b3a1cd670a1e927436ff
leon van der Stel <leonstel5@gmail.com>
2020-02-11 22:47:13 +0100

    update

--- globals.py
+++ globals.py
@@ -5,7 +5,13 @@ from selenium import webdriver
 """ File contains global variables """
 
 browser = webdriver.Chrome()
+
+# collects all the extacted matches which will be used for processing at the end of the scraping process
 matches_df = pd.DataFrame()
+
+# Right after the instertion of the player into the database its database id will be saved into the dictionary
+# so that later code could retrieve the database player ids in a fast and easy manner.
+# Without these the database must be selecting players constantly, NOT GOOD
 player_id_cache = {}
 
 # method for visiting new pages with selenium and then extract the new page's content with Beautiful Soup
